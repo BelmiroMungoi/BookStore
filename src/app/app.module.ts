474d17dc90379b24cadbb95131c2ff073b194601
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-13 15:25:55 +0200

    declarando um novo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { ClienteAddComponent } from './cliente-add/cliente-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent
+    ClienteComponent,
+    ClienteAddComponent
   ],
   imports: [
     BrowserModule,
