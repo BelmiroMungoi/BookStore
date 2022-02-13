fb90f48f2107879d97aed7fccdf37038899bad3c
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-13 18:05:38 +0200

    declarando o objeto cliente

--- src/app/cliente-add/cliente-add.component.ts
+++ src/app/cliente-add/cliente-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../model/cliente';
 
 @Component({
   selector: 'app-cliente-add',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteAddComponent implements OnInit {
 
+  cliente = new Cliente();
+  
   constructor() { }
 
   ngOnInit(): void {
